b5cd8b1ca49f3aede3bcfe75b08708532a1a2999
soapmanu <joseemanueljimenez@gmail.com>
2023-03-04 20:06:50 -0300

    rewriting

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ screen = pygame.display.set_mode((screen_width,screen_height))
 clock = pygame.time.Clock()
 level = Level(level_0,screen)
 
-
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
