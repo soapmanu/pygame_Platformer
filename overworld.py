bac3c2707cf58062efc886e556c780df2f5d32a3
soapmanu <joseemanueljimenez@gmail.com>
2023-03-09 06:34:19 -0300

    Finish the overworld logic with level selection

--- overworld.py
+++ overworld.py
@@ -2,7 +2,7 @@ import pygame
 from game_data import levels
 
 class Node(pygame.sprite.Sprite):
-    def __init__(self,pos,status):
+    def __init__(self,pos,status,icon_speed):
         super().__init__()
         self.image = pygame.Surface((100,80))
         if status == 'available':
@@ -10,21 +10,33 @@ class Node(pygame.sprite.Sprite):
         else:
             self.image.fill('grey')
         self.rect = self.image.get_rect(center = pos)
+        
+        self.detection_zone = pygame.Rect(self.rect.centerx - (icon_speed / 2),self.rect.centery - (icon_speed / 2),icon_speed,icon_speed)
     
 class Icon(pygame.sprite.Sprite):
     def __init__(self,pos):
         super().__init__()
+        self.pos = pos
         self.image = pygame.Surface((20,20))
         self.image.fill('blue')
         self.rect = self.image.get_rect(center = pos)
+        
+    def update(self):
+        self.rect.center = self.pos
           
 class Overworld:
-    def __init__(self,start_level,max_level,surface):
+    def __init__(self,start_level,max_level,surface,create_level):
         
         # Setup
         self.display_surface = surface
         self.max_level = max_level
         self.current_level = start_level
+        self.create_level = create_level
+        
+        # movement logic
+        self.move_direction = pygame.math.Vector2(0,0)
+        self.speed = 8
+        self.moving = False
         
         # sprites
         self.setup_nodes()
@@ -35,9 +47,9 @@ class Overworld:
         
         for index,node_data in enumerate(levels.values()):
             if index <= self.max_level:
-                node_sprite = Node(node_data['node_pos'],'available')
+                node_sprite = Node(node_data['node_pos'],'available',self.speed)
             else:
-                node_sprite =Node(node_data['node_pos'],'locked')
+                node_sprite =Node(node_data['node_pos'],'locked',self.speed)
             self.nodes.add(node_sprite)
             
     def draw_paths(self):
@@ -52,18 +64,41 @@ class Overworld:
     def input(self):
         keys = pygame.key.get_pressed()
         
-        if keys[pygame.K_RIGHT] and self.current_level < self.max_level:
-            self.current_level += 1
-        elif keys[pygame.K_LEFT] and self.current_level > 0:
-            self.current_level -= 1
+        if not self.moving:
+            if keys[pygame.K_RIGHT] and self.current_level < self.max_level:
+                self.move_direction = self.get_movement_data('next')
+                self.current_level += 1
+                self.moving = True
+            elif keys[pygame.K_LEFT] and self.current_level > 0:
+                self.move_direction = self.get_movement_data('previous')
+                self.current_level -= 1
+                self.moving = True
+            elif keys[pygame.K_SPACE]:
+                self.create_level(self.current_level)
+    
+    def get_movement_data(self,target):
+        start = pygame.math.Vector2(self.nodes.sprites()[self.current_level].rect.center)
         
+        if target == 'next':
+            end = pygame.math.Vector2(self.nodes.sprites()[self.current_level + 1].rect.center)
+        else:
+            end = pygame.math.Vector2(self.nodes.sprites()[self.current_level - 1].rect.center)
+        
+        return (end - start).normalize()
+       
     def update_icon_pos(self):
-        pass
-        #self.icon.sprite.rect.center = self.nodes.sprites()[self.current_level].rect.center
+        if self.moving and self.move_direction:
+            self.icon.sprite.pos += self.move_direction * self.speed
+            target_node = self.nodes.sprites()[self.current_level]
+            if target_node.detection_zone.collidepoint(self.icon.sprite.pos):
+                self.moving = False
+                self.move_direction = pygame.math.Vector2(0,0)
         
     def run(self):
         self.input()
+        self.update_icon_pos()
+        self.icon.update()
         self.draw_paths()
         self.nodes.draw(self.display_surface)
         self.icon.draw(self.display_surface)
-        self.update_icon_pos()
+        
