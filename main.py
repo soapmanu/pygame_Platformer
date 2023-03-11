b32ab5783977a1172cabbd137e36ee80895e9b66
soapmanu <joseemanueljimenez@gmail.com>
2023-03-11 05:31:00 -0300

    Added the cloud and palms to the overworld menu

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from overworld import Overworld
 
 class Game:
     def __init__(self):
-        self.max_level = 3
-        self.overworld = Overworld(1,self.max_level,screen,self.create_level)
+        self.max_level = 0
+        self.overworld = Overworld(0,self.max_level,screen,self.create_level)
         self.status = 'overworld'
         
     def create_level(self,current_level):
--- overworld.py
+++ overworld.py
@@ -59,7 +59,7 @@ class Overworld:
         # sprites
         self.setup_nodes()
         self.setup_icon()
-        self.sky = Sky(8)
+        self.sky = Sky(8,'overworld')
         
     def setup_nodes(self):
         self.nodes = pygame.sprite.Group()
@@ -72,8 +72,9 @@ class Overworld:
             self.nodes.add(node_sprite)
             
     def draw_paths(self):
-        points = [node['node_pos'] for index,node in enumerate(levels.values()) if index <= self.max_level]
-        pygame.draw.lines(self.display_surface,'#a04f45',False,points,6)
+        if self.max_level > 0:
+            points = [node['node_pos'] for index,node in enumerate(levels.values()) if index <= self.max_level]
+            pygame.draw.lines(self.display_surface,'#a04f45',False,points,6)
        
     def setup_icon(self):
         self.icon = pygame.sprite.GroupSingle()
